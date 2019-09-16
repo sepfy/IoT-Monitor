2422440d1af5777addce905029cbfbb47c80769f
sepfy <sepfy95@gmail.com>
2019-09-16 22:36:05 +0800

    modify setting

--- recovery.py
+++ recovery.py
@@ -5,6 +5,5 @@ db.put("admin", "123456")
 
 devdb = LevelDBModel("devinfo")
 devdb.put_dict("Gateway", {"type": "Thermometer", "location": "Living Room"})
-devdb.put_dict("120932ddwa", {"type": "Thermometer", "location":"Outdoor"})
 
 
